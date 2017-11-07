0859690521969a0d0a378f135ff55b89b4ba3feb
unknown <duytrung177@gmail.com>
2017-11-07 14:24:42 +0700

    update pages pruduct

--- myjs/offers.js
+++ myjs/offers.js
@@ -1,10 +1,30 @@
 var app = angular.module("myFirstApp",[]);
+//get data suzuki for offer
 app.controller('myCtr', ['$scope', '$http', function ($scope, $http) {
     $scope.suzukies = [];
+    $scope.yamahaes = [];
+    $scope.hondaes = [];
+    $scope.harleys = [];
     $http
-        .get("data/dataoffer.json")
+        .get("data/suzukidata.json")
         .then(function(response){
             $scope.suzukies = response.data.suzuki;
         });
+    $http
+        .get("data/yamahadata.json")
+        .then(function(response){
+            $scope.yamahaes = response.data.yamaha;
+        });
+    $http
+        .get("data/hondadata.json")
+        .then(function(response){
+            $scope.hondaes = response.data.honda;
+        });
+    $http
+        .get("data/harleydavidsondata.json")
+        .then(function(response){
+            $scope.harleys = response.data.harley;
+        });
 }]);
+
 
